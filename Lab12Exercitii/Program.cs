7d2b207991239f9204bdbfd4e2da2dd4ef0f688c
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-05-22 11:36:01 +0300

    Add project files.

---
+++ Lab12Exercitii/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab12Exercitii;
+
+
+try
+{
+    Lab12Exercitii.Queue<int> intQue = new Lab12Exercitii.Queue<int>(1);
+    try
+    {
+        intQue.Enqueue(1);
+        Console.WriteLine("Items enQued successfully.");
+        intQue.Enqueue(2);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while enQueing: {ex.Message}");
+    }
+    try
+    {
+        Console.WriteLine($"DeQued: {intQue.Dequeue()}");
+        Console.WriteLine($"DeQued: {intQue.Dequeue()}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while deQueing: {ex.Message}");
+    }
+
+    Lab12Exercitii.Queue<string> stringQue = new Lab12Exercitii.Queue<string>(1);
+
+    try
+    {
+        stringQue.Enqueue("one");
+        Console.WriteLine("String items enQued successfully.");
+
+        stringQue.Enqueue("two");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while enQueing string items: {ex.Message}");
+    }
+
+    try
+    {
+        Console.WriteLine($"DeQued: {stringQue.Dequeue()}");
+        Console.WriteLine($"DeQued: {stringQue.Dequeue()}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while deQueing string items: {ex.Message}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error: {ex.Message}");
+}
---
+++ Lab12Exercitii/Queue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12Exercitii
+{
+    internal class Queue<T>
+    {
+        private T[] items;
+        private int capacity;
+        private int head;
+        private int tail;
+        private int count;
+        public Queue(int capacity)
+        {
+            this.capacity = capacity;
+            items = new T[capacity];
+            head = 0;
+            tail = 0;
+            count = 0;
+        }
+        public void Enqueue(T item)
+        {
+            if (IsFull())
+            {
+                throw new Exception("Queue is full");
+            }
+
+            items[tail] = item;
+            tail = (tail + 1) % capacity;
+            count++;
+        }
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Queue is empty");
+            }
+
+            T item = items[head];
+            head = (head + 1) % capacity;
+            count--;
+            return item;
+        }
+        public int Count()
+        {
+            return count;
+        }
+        private bool IsFull()
+        {
+            return count == capacity;
+        }
+        private bool IsEmpty()
+        {
+            return count == 0;
+        }
+    }
+}
